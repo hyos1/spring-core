78ba817cc4aac159bae0039edf836eda07fe7c9c
hyos <hyozzang99@gmail.com>
2025-07-04 21:54:54 +0900

    Study: AppConfig를 통한 의존관계 주입, 테스트 코드에도 적용

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -4,7 +4,9 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 
 public class RateDiscountPolicy implements DiscountPolicy {
-    private int discountPercent = 10; //10% 할인
+
+    private int discountPercent = 10;
+
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -7,6 +7,7 @@ public class MemberServiceImpl implements MemberService {
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
